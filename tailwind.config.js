006b0df7ca187cb04f4afb1319ac8e29700b0cf2
Bekir ŞAFAK <bekir_safak_38@hotmail.com>
2023-07-01 00:56:44 +0300

    Tailwind kullanılarak site yazıldı. Geliştirme devam ediyor.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      // * Burada fontawesome uçak iconu için animasyon yazıldı.
+      keyframes: {
+        flying: {
+          '0%, 100%': { transform: 'rotate(-45deg)' },
+        },
+        animation: {
+          flying: 'wiggle 1s ease-in-out infinite',
+        },
+      }
+    },
   },
   plugins: [],
 }
