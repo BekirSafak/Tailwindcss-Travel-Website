70a647917c8af525556e93c0a626383706cf04bf
Bekir ŞAFAK <bekir_safak_38@hotmail.com>
2023-07-01 20:13:53 +0300

    Tailwind ile uçak resmine animasyon eklendi.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,13 @@ module.exports = {
         animation: {
           flying: 'wiggle 1s ease-in-out infinite',
         },
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(1deg)' },
+          '50%': { transform: 'rotate(0deg)' },
+        },
+        animation: {
+          wiggle: 'wiggle 1s ease-in-out infinite',
+        }
       }
     },
   },
